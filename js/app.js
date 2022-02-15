dc61637c68b17d0457f1cd4cc8102644b43dc122
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-15 13:51:23 +0600

    verify match

--- js/app.js
+++ js/app.js
@@ -33,4 +33,26 @@ document.getElementById('key-pad').addEventListener('click', function(event){
 
     }
     
-})
+});
+
+
+function verifyPin(){
+    // console.log('clicked');
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('type-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+
+    if(pin == typedNumbers){
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+
+        
+    }
+    else{
+        failError.style.display = 'block';
+        successMessage.style.display = 'none';
+        
+    }
+    
+}
